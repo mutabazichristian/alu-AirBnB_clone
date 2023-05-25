98cfc59147bc5bef3c23b1f4a3f70da8c5ba278c
mutabazichristian <c.mutabazi@alustudent.com>
2023-05-25 11:03:03 +0200

    linking BaseModel instances to Filestorage class in models/engine, saving and loading is handled by storage variable

--- models/base_model.py
+++ models/base_model.py
@@ -32,15 +32,17 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    setattr(self, key, value)
+                setattr(self, key, value)
         else:
 
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """
