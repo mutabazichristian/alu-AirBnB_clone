55025594aeb1af866d2763d701e681a4e2b6e956
mutabazichristian <c.mutabazi@alustudent.com>
2023-05-25 09:22:51 +0200

    adding kwargs in the init function

--- models/base_model.py
+++ models/base_model.py
@@ -21,16 +21,24 @@ class BaseModel:
 
     """
 
-    def __init__(self):
+    def __init__(self,*args,**kwargs):
         """
         Initializes a new instance of the BaseModel class.
         - Assigns a unique UUID to the `id` attribute.
         - Sets the `created_at` attribute to the current datetime.
         - Sets the `updated_at` attribute to the `created_at` value.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
