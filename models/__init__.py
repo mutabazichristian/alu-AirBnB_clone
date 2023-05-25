70a78258069ac99f1a247765389f14b62db3f827
mutabazichristian <c.mutabazi@alustudent.com>
2023-05-25 10:42:11 +0200

    updating models/__init__.py with imports for FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
 
 classes = {"BaseModel": BaseModel}
