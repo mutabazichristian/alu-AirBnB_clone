97bda4a21da5123e85480025e3660419d61b5d30
mutabazichristian <c.mutabazi@alustudent.com>
2023-05-25 09:40:31 +0200

    task 4, intranet check

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+
+classes = {"BaseModel": BaseModel}
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import uuid
+import models
 from datetime import datetime
 
 class BaseModel:
